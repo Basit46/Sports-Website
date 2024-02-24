eb7a97bbfe856a6f70d1e4e3126be0456fc43ac8
Basit46 <Hassanbasit1990@gmail.com>
2024-02-24 22:40:13 +0100

    Structured Navbar

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDk84JWuXnuJ5iNCy_c-V8u7m27f4DnKYY",
+//   authDomain: "sport-site-d1601.firebaseapp.com",
+//   projectId: "sport-site-d1601",
+//   storageBucket: "sport-site-d1601.appspot.com",
+//   messagingSenderId: "675884158669",
+//   appId: "1:675884158669:web:bf3158392be874fd43e384",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,6 +4,8 @@ import { LeagueCodes, fetchDataForLeagues } from "../utils/fetchDataforLeagues";
 import { useGlobalContext } from "../context/globalContext";
 import { Link } from "react-router-dom";
 import Stat from "../components/Stat";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const Hero = () => {
   const { article } = useGlobalContext();
@@ -36,75 +38,83 @@ const Hero = () => {
 
   return (
     <section className="h-[356px] flex gap-[24px]">
-      <div className="relative w-[65%] rounded-[16px] overflow-hidden">
-        <img
-          className="h-full w-full object-cover"
-          src={article?.images[0]?.url}
-          alt="Article"
-        />
-
-        <div className="absolute top-0 left-0 w-full h-full flex items-end">
-          <div className="bg-black/70 w-full px-[10px] py-[5px] flex flex-col justify-between gap-[10px] items-start">
-            <p className="text-[20px] font-medium leading-[1.2]">
-              {article?.headline}
-            </p>
-            <Link
-              to={`articles/${article?.dataSourceIdentifier}/${article?.headline}`}
-              className="underline text-[#4b4bf5]"
-            >
-              Read More
-            </Link>
-          </div>
-        </div>
-      </div>
-
-      <div className="max-h-[356px] h-fit flex-1 bg-[#1B1C21] rounded-[16px] py-[26px] px-[22px]">
-        <p className="font-[600] text-center">
-          {match?.status.type.state == "pre" && "Coming soon"}
-          {match?.status.type.state == "in" && "Live Match"}
-          {match?.status.type.state == "post" && "Match Ended"}
-        </p>
-        <p className="text-center text-[14px] font-medium text-[#14FF00]">
-          {match?.status.type.state == "pre" &&
-            new Date(match.date).toLocaleTimeString([], {
-              hour: "numeric",
-              minute: "numeric",
-              hour12: true,
-            })}
-          {match?.status.type.state == "in" && match.status.displayClock}
-          {match?.status.type.state == "post" && "FT"}
-        </p>
-
-        <div className="my-[20px] flex justify-between items-center">
+      {article ? (
+        <div className="relative h-full w-[65%] rounded-[16px] overflow-hidden">
           <img
-            className="h-[56px] w-[56px]"
-            src={match?.competitions[0].competitors[0].team.logo}
-            alt="Team logo"
+            className="h-full w-full object-cover"
+            src={article?.images[0]?.url}
+            alt="Article"
           />
-          {match?.status.type.state != "pre" ? (
-            <div className="py-[8px] px-[16px] rounded-[50px] bg-[#F5C451]/10 text-[#F5C451] text-[18px] font-[600]">
-              {match?.competitions[0].competitors[0].score} -{" "}
-              {match?.competitions[0].competitors[1].score}
+
+          <div className="absolute top-0 left-0 w-full h-full flex items-end">
+            <div className="bg-black/70 w-full px-[10px] py-[5px] flex flex-col justify-between gap-[10px] items-start">
+              <p className="text-[20px] font-medium leading-[1.2]">
+                {article?.headline}
+              </p>
+              <Link
+                to={`articles/${article?.dataSourceIdentifier}/${article?.headline}`}
+                className="underline text-[#4b4bf5]"
+              >
+                Read More
+              </Link>
             </div>
-          ) : (
-            <div> VS </div>
-          )}
-
-          <img
-            className="h-[56px] w-[56px]"
-            src={match?.competitions[0]?.competitors[1]?.team?.logo}
-            alt="Team logo"
-          />
-        </div>
-
-        {match?.status.type.state != "pre" && (
-          <div className="flex flex-col gap-[16px]">
-            <Stat match={match} title="Possession" statIndex={4} />
-            <Stat match={match} title="Shot" statIndex={8} />
-            <Stat match={match} title="Shot On Target" statIndex={6} />
           </div>
-        )}
-      </div>
+        </div>
+      ) : (
+        <div className="relative h-full bg-[#1B1C21] w-[65%] rounded-[16px] overflow-hidden" />
+      )}
+
+      {match ? (
+        <div className="max-h-full h-fit flex-1 bg-[#1B1C21] rounded-[16px] py-[26px] px-[22px]">
+          <p className="font-[600] text-center">
+            {match?.status.type.state == "pre" && "Coming soon"}
+            {match?.status.type.state == "in" && "Live Match"}
+            {match?.status.type.state == "post" && "Match Ended"}
+          </p>
+          <p className="text-center text-[14px] font-medium text-[#14FF00]">
+            {match?.status.type.state == "pre" &&
+              new Date(match.date).toLocaleTimeString([], {
+                hour: "numeric",
+                minute: "numeric",
+                hour12: true,
+              })}
+            {match?.status.type.state == "in" && match.status.displayClock}
+            {match?.status.type.state == "post" && "FT"}
+          </p>
+
+          <div className="my-[20px] flex justify-between items-center">
+            <img
+              className="h-[56px] w-[56px]"
+              src={match?.competitions[0].competitors[0].team.logo}
+              alt="Team logo"
+            />
+            {match?.status.type.state != "pre" ? (
+              <div className="py-[8px] px-[16px] rounded-[50px] bg-[#F5C451]/10 text-[#F5C451] text-[18px] font-[600]">
+                {match?.competitions[0].competitors[0].score} -{" "}
+                {match?.competitions[0].competitors[1].score}
+              </div>
+            ) : (
+              <div> VS </div>
+            )}
+
+            <img
+              className="h-[56px] w-[56px]"
+              src={match?.competitions[0]?.competitors[1]?.team?.logo}
+              alt="Team logo"
+            />
+          </div>
+
+          {match?.status.type.state != "pre" && (
+            <div className="flex flex-col gap-[16px]">
+              <Stat match={match} title="Possession" statIndex={4} />
+              <Stat match={match} title="Shot" statIndex={8} />
+              <Stat match={match} title="Shot On Target" statIndex={6} />
+            </div>
+          )}
+        </div>
+      ) : (
+        <Skeleton className="h-[356px]" baseColor="red" borderRadius={16} />
+      )}
     </section>
   );
 };
